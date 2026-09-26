1dab32a8bd56f4f33d57eb59e2e6a3c7802c9e78
spkesDE <1682314+spkesde@users.noreply.github.com>
2026-09-26 15:09:14 +0200

    Prepare release v12.7.1.0

--- src/constants.ts
+++ src/constants.ts
@@ -4,4 +4,4 @@ export const ADMIN_NAV_LINK_ATTR = 'data-jellyfin-featured-admin-link';
 export const PLUGIN_DISPLAY_NAME = 'Jellyfin Featured';
 export const CONSOLE_PREFIX = '[Featured]';
 export const USER_PREFERENCES_CHANGED_EVENT = 'jellyfin-featured:preferences-changed';
-export const PLUGIN_VERSION = '12.7.0.0';
+export const PLUGIN_VERSION = '12.7.1.0';
